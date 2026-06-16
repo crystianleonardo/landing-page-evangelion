64716661f9f8b19dc5a17299200d6f588c737c27
crys_kriss <crystianleonardo49@gmail.com>
2026-06-16 15:25:24 -0300

    Add files via upload

---
+++ landing-evangelion/src/App.jsx
@@ -0,0 +1,9 @@
+/*import reactLogo from './assets/react.svg' import viteLogo from './assets/vite.svg' import heroImg from './assets/hero.png' */
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div></div>
+  )
+}
---
+++ landing-evangelion/src/paginas/landingpage.jsx
@@ -0,0 +1,10 @@
+import estilos from './landingpage.module.css'
+
+export function LandingPage() {
+
+    return (
+        <div className={estilos.conteiner}>
+            <h1 className={estilos.titulo}>Evangelio kkk</h1>
+        </div>
+    )
+}
